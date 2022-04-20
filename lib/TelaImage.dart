95562d9a5dac6dc1c338857a5d3d92d58bf87226
murilo <murilra@hotmail.com>
2022-04-20 10:11:02 -0300

    Initial commit

---
+++ lib/TelaImage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TelaImage extends StatefulWidget {
+  const TelaImage({Key? key}) : super(key: key);
+
+  @override
+  State<TelaImage> createState() => _TelaImageState();
+}
+
+class _TelaImageState extends State<TelaImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Teste de input image'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+                'https://png.pngtree.com/png-vector/20210222/ourlarge/pngtree-letter-m-3d-logo-design-png-png-image_2933784.jpg'),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder()
+              ),
+            )
+          ],
+        ),
+      ),)
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'TelaImage.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: TelaImage(),
+  ));
+}
